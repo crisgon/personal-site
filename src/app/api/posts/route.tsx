8276e217664d1f6402a83ddd17fe3a0de16ec8d4
Cris <kristiano.gon@gmail.com>
2025-06-26 11:48:00 -0300

    fix: fix build error

--- src/app/api/posts/route.tsx
+++ src/app/api/posts/route.tsx
@@ -1,9 +1,8 @@
 import { getAllPosts } from "@/lib/blog";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, response: Response) {
+export async function GET() {
   const allPosts = getAllPosts();
 
   return NextResponse.json({ data: allPosts });
-  // return NextResponse.json({ data: allPosts }, { status: 200 });
 }
