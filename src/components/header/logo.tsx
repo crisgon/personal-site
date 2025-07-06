72d84881f4f7f2312eeff8ea344590a44b37fbaf
Cris <kristiano.gon@gmail.com>
2025-07-06 17:38:09 -0300

    feat: use Link on logo header

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export function Logo() {
   return (
-    <a
+    <Link
       href="/"
       className={
         "font-bungee text-black text-2xl flex items-center justify-center bg-white rounded-xl w-10 h-10 hover:bg-gradient-to-r from-orange-600 to-pink-600"
       }
     >
       C
-    </a>
+    </Link>
   );
 }
